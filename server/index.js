8bafea61e4075b75f8908909094920dad79e2e22
ManavPratapSingh-off <manav3407.gb@gmail.com>
2025-10-22 17:13:44 +0530

    feat: update CORS configuration to use environment variable for client URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
